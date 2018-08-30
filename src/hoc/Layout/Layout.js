e1881ff50630904c9cc1e3388ba59e7ccf83640b
Andy <andrew.rutherford.6@gmail.com>
2018-08-30 15:07:37 +0300

    reordered file structure

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import Wrapper from '../../hoc/Wrapper';
+import Wrapper from '../Wrapper/Wrapper';
 import classes from './Layout.css';
-import Toolbar from '../Navigation/Toolbar/Toolbar';
-import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
     state = {
@@ -17,7 +17,7 @@ class Layout extends Component {
              return {showSideDrawer: !prevState.showSideDrawer}
         });
     }
-
+ 
     render () {
         return (
             <Wrapper>
